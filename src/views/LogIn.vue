7b5dfe2998c3bc3da1081dbebcf2054ad3fa19ec
mingri31164 <3116430062@qq.com>
2024-12-17 07:37:29 +0800

    chore：修复已登录状态下登录页面的错误跳转

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -69,6 +69,9 @@ export default {
   },
   mounted() {
     document.title = "Shopping"
+    if (this.$store.getters.getUserInfo.state.user.userId){
+      this.$router.push('/')
+    }
   },
   methods: {
     changeType() {
